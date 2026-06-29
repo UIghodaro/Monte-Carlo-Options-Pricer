7524cc5a588b385d69a8ba5496d5722b5cb31750
Uyi Ighodaro <141790084+UIghodaro@users.noreply.github.com>
2026-06-29 18:13:29 +0100

    Update simulation.cpp
    Introduce Monte-Carlo Methods to the script as well as outputs. Next stop is multithreading via threadpool! We'll see how much I remember.

--- simulation.cpp
+++ simulation.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cmath>
+#include <random>                                          
+
 // compilation: g++ -O3 -o simulation.out simulation.cpp 
 // running:     ./simulation.out
 
@@ -22,7 +24,7 @@ double CDF(double x) {
 double BlackScholesMertonCall(double S, double K, double r, double T, double sigma) {
     // Needs to calculate d1, d2, their CDFs, then you can just plug in numbers? Or am I allowing time variation?
 
-    double BSM;                                                   // Eventual output
+    double call;                                                    // Eventual output
 
     double visor = sigma*std::sqrt(T);                              // Separate out here due to being involved in both
 
@@ -32,21 +34,54 @@ double BlackScholesMertonCall(double S, double K, double r, double T, double sig
     double d2 = d1 - visor;
 
     // Use the CDF function with these to compute Black-Scholes
-    BSM = S*CDF(d1) - K*std::exp(-r*T)*CDF(d2);
+    call = S*CDF(d1) - K*std::exp(-r*T)*CDF(d2);
     
-    return BSM;
+    return call;
 }
 
 // Letting N being number of Monte-Carlo epochs
 // This needs to use the fundamental Brownian motion equation which the Black-Scholes-Merton equation finds the closed form expectation of
 // Monte-Carlo methods just abuses the Law of Large Numbers
 
-// We use the GBM equation: S(T) = S(0)*exp()
+// We use the GBM equation: S(T) = S(0)*exp((r - sigma^2/2)T + sigma*sqrt{T}*Z(i)) over i from 1 -> N iterations
+// Optimise I guess, S(T) = S(0) * exp((r - sigma^2/2)T + sigma*sqrt{T}*Z(i)) = S(0)*exp((r - sigma^2/2)*T)* exp(sigma*sqrt{T}*Z(i))
+
+// Z here is a value picked from a normal distribution of mean 0 and variance 1
 double monteCarloCall(double S, double K, double r, double T, double sigma, int N) {
-    return 0;
+    // Generate N values for Z, then compute S(T) at that value of Z
+
+    double accum = 0.0;                                                     // Cumulative S(T) value, divide by N after
+    double call;                                                            // Eventual output
+    
+    // Generates Z values
+    std::random_device rd;
+    std::mt19937 gen(rd());                                                 // 'Mersenne Twister' apparently good
+    std::normal_distribution<double> dist(0, 1);                            // Holy acrobatics
+    
+    // Variables for compute with Z, reduce power of the for loop
+    double outerMult = S*std::exp((r - (sigma*sigma/2))*T);                 // This optimises into a number, far easier compute overall
+    double innerMult = sigma*std::sqrt(T);                                  // Same line of reasoning
+    
+    for (int i = 0; i < N; i++) {
+        double Z = dist(gen);
+        accum += std::fmax(outerMult*std::exp(innerMult*Z) - K, 0);         // accumulate MAX(S(T) - K, 0)
+    }
+
+    call = std::exp(-r * T)*(accum/N);
+
+    return call;
 }
 
-int main(double S, double K, double r, double T, double sigma) {
-    std::cout << "The Black-Scholes-Merton output of this configuration is: " << BlackScholesMertonCall(1,1,1,1,1) << std::endl;
+int main() {
+    // Configurable inputs
+    double S = 1; double K = 1; double r = 1; double T = 1; double sigma = 1; 
+    //int N = 100000; 
+
+    std::cout << "The Black-Scholes-Merton output of this configuration is: " << BlackScholesMertonCall(S,K,r,T,sigma) << std::endl;
+    //std::cout << "The Monte-Carlo output of this configuration over " << N << " epochs is: " << monteCarloCall(S,K,r,T,sigma, N) << std::endl;
+    std::cout << "The Monte-Carlo output of this configuration over 1000 epochs is: " << monteCarloCall(S, K, r, T, sigma, 1000) << "\n";
+    std::cout << "The Monte-Carlo output of this configuration over 10000 epochs is: " << monteCarloCall(S, K, r, T, sigma, 10000) << "\n";
+    std::cout << "The Monte-Carlo output of this configuration over 100000 epochs is: " << monteCarloCall(S, K, r, T, sigma, 100000) << "\n";
+    std::cout << "The Monte-Carlo output of this configuration over 1000000 epochs is: " << monteCarloCall(S, K, r, T, sigma, 1000000) << "\n";
     return 0;
 }
